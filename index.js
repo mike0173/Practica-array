9aa17fc255911dc59e9fc750c20bd43ee7bdb812
mike <miguelagb070@gmail.com>
2024-03-05 12:23:49 -0500

    array repetir

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ let carros = ["ferrari", "audi","maserati"];
 let variado = ["leon", 10, true];
 let frutas = new Array ("manzana", "pera", "banano", "piña");
 let numerito = Array.from ({ length: 5 }, (_, index) => index);
+let repetir =Array(7). fill (0);
