a29fdb899fcb20af297d1dda6af03721dd4e9392
mike <miguelagb070@gmail.com>
2024-03-05 12:07:53 -0500

    array numerico

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ let instruments = [];
 let carros = ["ferrari", "audi","maserati"];
 let variado = ["leon", 10, true];
 let frutas = new Array ("manzana", "pera", "banano", "piña");
+let numerito = Array.from ({ length: 5 }, (_, index) => index);
